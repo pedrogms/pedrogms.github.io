74f7c9ecd066a3e762f84c6418888b002114b8f9
Pedro Silva <grutslim3000@gmail.com>
2021-03-26 17:10:19 -0300

    New logotype and new style in card project

--- js/components/main-carousel.js
+++ js/components/main-carousel.js
@@ -1,16 +1,12 @@
-
-const btnRight = document.querySelector('.arrow.-right')
-const btnLeft = document.querySelector('.arrow.-left')
-const elements = document.querySelector('.elements')
-let pixel = 100
+const btnRight = document.querySelector('.arrow.-right');
+const btnLeft = document.querySelector('.arrow.-left');
+const elements = document.querySelector('.elements');
+let pixel = 100;
 
 btnRight.addEventListener('click', function(){
     pixel = pixel + 100;
     elements.style = `transform: translateX(${pixel}px)`;
 });
-
-
-
 btnLeft.addEventListener('click', function(){
     pixel = pixel - 100;
     elements.style = `transform: translateX(${pixel}px)`
